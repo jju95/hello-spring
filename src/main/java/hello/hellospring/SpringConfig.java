ba20b51de7477000af2e103a29a0539515a9403c
rlagmlwn9510 <rlagmlwn9510@gmail.com>
2021-01-20 17:19:04 +0900

    21/01-20

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,16 +1,28 @@
 package hello.hellospring;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.batch.BatchDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.jdbcMemberRepository;
 import hello.hellospring.service.MemberService;
+import lombok.Data;
 
 @Configuration // 스프링 컨피그
 public class SpringConfig {
  	
+	DataSource dataSource;
+	
+	@Autowired
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
@@ -18,7 +30,8 @@ public class SpringConfig {
 	
 	@Bean 
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+		//return new MemoryMemberRepository();
+		return new jdbcMemberRepository(dataSource);
 	}
 	
 }
